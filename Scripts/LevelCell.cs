fc3191875a1425337e4a681ef1a5f7cb7b0888af
nicholasmatthews-dev <nicholasjaymatthews@gmail.com>
2023-11-18 14:24:39 -0700

    Added documentation to LevelCell

--- Scripts/LevelCell.cs
+++ Scripts/LevelCell.cs
@@ -50,6 +50,9 @@ public partial class LevelCell : Node2D
 	{
 	}
 
+	/// <summary>
+	/// Updates the solid array by reading the data of the given cells.
+	/// </summary>
 	public void CheckSolid(){
 		for (int i=0; i < Width; i++){
 			for (int j=0; j < Height; j++){
@@ -68,6 +71,12 @@ public partial class LevelCell : Node2D
 		}
 	}
 
+	/// <summary>
+	/// Checks if a collection of tiles is a valid placement (doesn't collide with any
+	/// tiles marked solid).
+	/// </summary>
+	/// <param name="occupied">The collection of tiles to check collision for.</param>
+	/// <returns><c>True</c> if none of the tiles are solid, <c>False</c> otherwise.</returns>
 	public bool PositionValid(ICollection<Vector2I> occupied){
 		bool valid = true;
 		foreach (Vector2I position in occupied){
@@ -80,6 +89,13 @@ public partial class LevelCell : Node2D
 		return valid;
 	}
 
+	/// <summary>
+	/// Places a tile on this cell's tile map.
+	/// </summary>
+	/// <param name="layer">The layer to place the tile on.</param>
+	/// <param name="tileSetRef">The reference id for the tileset to be used.</param>
+	/// <param name="coords">The position to place the tile on.</param>
+	/// <param name="atlasCoords">The atlas coordinates of the tile to draw.</param>
 	public void Place(int layer, int tileSetRef, Vector2I coords, Vector2I atlasCoords){
 		if (!tickets.ContainsKey(tileSetRef)){
 			AddTicket(tileSetRef);
@@ -93,12 +109,26 @@ public partial class LevelCell : Node2D
 		);
 	}
 
+	/// <summary>
+	/// Requests a ticket for a given <c> tileSetRef </c> and then adds the ticket
+	/// to the list of active tickets. Additionally stores a dictionary entry to associate
+	/// the atlas id of the ticket with the given <c> tileSetRef </c> mainly used for
+	/// <see cref="GetTileIdentifier"/> 
+	/// </summary>
+	/// <param name="tileSetRef">The reference code for the desired tileset.</param>
 	private void AddTicket(int tileSetRef){
 		TileSetTicket tileSetTicket = tileSetManager.GetTileSetTicket(tileSetRef);
 		tickets.Add(tileSetRef, tileSetTicket);
 		atlasCodesToRef.Add(tileSetTicket.GetAtlasId(), tileSetRef);
 	}
 
+	/// <summary>
+	/// TODO: Implement full functionality.
+	/// <para>
+	/// Returns a representation of this LevelCell as an ordered array of bytes.
+	/// </para>
+	/// </summary>
+	/// <returns>An ordered byte array representing this object.</returns>
 	public byte[] Serialize(){
 		Dictionary<(int, Vector2I), int> uniqueTiles = CountUniqueTiles();
 		int tileBits = (int)Math.Ceiling(Math.Log2(uniqueTiles.Count));
@@ -128,6 +158,15 @@ public partial class LevelCell : Node2D
 		return new byte[1];
 	}
 
+	/// <summary>
+	/// Returns all tiles as a linear list of tile identifier codes 
+	/// (see <see cref="CountUniqueTiles"/>).
+	/// <para>
+	/// Follows the ordering in terms of blocks Layer->Row->Column->Tile.
+	/// </para>
+	/// </summary>
+	/// <param name="uniqueTiles">A dictionary of unique tiles and their tile identifier codes.</param>
+	/// <returns>A list of tile identifier codes as described above.</returns>
 	private List<int> GetTilesAsCodeArray(Dictionary<(int, Vector2I), int> uniqueTiles){
 		List<int> tileCodes = new List<int>();
 		for (int i = 0; i < numLayers; i++){
@@ -142,6 +181,13 @@ public partial class LevelCell : Node2D
 		return tileCodes;
 	}
 
+	/// <summary>
+	/// Returns the tile identifier for a specific tile. Represented by a tuple with the
+	/// following format <c> (int tileSetRef, Vector2I atlasCoords) </c>
+	/// </summary>
+	/// <param name="layer">The layer of the specified tile.</param>
+	/// <param name="coords">The position of the specified tile.</param>
+	/// <returns>A tuple with the format specified above.</returns>
 	private (int, Vector2I) GetTileIdentifier(int layer, Vector2I coords){
 		int sourceId = tiles.GetCellSourceId(layer, coords);
 		if (sourceId == -1){
@@ -152,6 +198,15 @@ public partial class LevelCell : Node2D
 		return (sourceRef, atlasCoords);
 	}
 
+	/// <summary>
+	/// Returns a dictionary which associates tile types (represented as a tuple with the 
+	/// following format: <c> (int tileSetRef, Vector2I atlasCoords) </c>) with a corresponding
+	/// integer which uniquely identifies that tile type.
+	/// <para>
+	/// Note that the unique tile (-1, (-1, -1)) is always assigned with the id 0.
+	/// </para>
+	/// </summary>
+	/// <returns>A dictionary with the above referenced structure.</returns>
 	private Dictionary<(int, Vector2I), int> CountUniqueTiles(){
 		int tileTypes = 0;
 		// All tiles and their corresponding library index, in the format (tileSetRef, atlasx, atlasy)
